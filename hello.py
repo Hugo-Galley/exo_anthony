7ba3047d95e40d7f315726a8bb80c8baee0e2bd5
Freaks <galleyhugo@icloud.com>
2024-02-08 12:50:29 +0100

    Ajout de l'année de naissance

--- hello.py
+++ hello.py
@@ -3,3 +3,7 @@
 prenom = input("Quel est votre prénom ? ")
 nom = input("Quel est votre nom ? ")
 age = int(input("Quel est votre âge ? "))
+
+anne_naissance = 2024 - age
+
+print("Bonjour", prenom, nom, "vous avez", age, "ans. Vous êtes né en", anne_naissance, ".")
